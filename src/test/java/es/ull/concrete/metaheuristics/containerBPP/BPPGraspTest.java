390b412de7debdd529b649c0d5ddee75b706173b
Eduardo José Barrios García <eduardojbarriosgarcia@gmail.com>
2023-12-18 13:35:36 +0000

    RE-STRUCTURE OF TESTS / INCREASE OF COVERAGE

---
+++ src/test/java/es/ull/concrete/metaheuristics/containerBPP/BPPGraspTest.java
@@ -0,0 +1,31 @@
+package es.ull.concrete.metaheuristics.containerBPP;
+
+import es.ull.abstracts.problem.ILoadingProblems;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class BPPGraspTest {
+
+    private BPPGrasp bppGrasp;
+    private ILoadingProblems problemInstance;
+
+    @BeforeEach
+    void setUp() {
+        // Aquí se debe inicializar 'problemInstance' con una implementación concreta o una simulada
+        // problemInstance = new ...;
+
+        bppGrasp = new BPPGrasp(problemInstance, 0.5, 10);
+    }
+
+    @Test
+    @DisplayName("Constructor initializes BPPGrasp correctly")
+    void testConstructor() {
+        assertNotNull(bppGrasp, "BPPGrasp should not be null after construction");
+    }
+
+
+    // Más pruebas pueden ser añadidas aquí si hay otros métodos públicos
+}
