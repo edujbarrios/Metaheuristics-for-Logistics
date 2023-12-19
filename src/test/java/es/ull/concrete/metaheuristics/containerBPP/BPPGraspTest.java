3890b7b834d4d0fd120e63908ea252da290137d1
pau-regalado <alu0101330174@ull.edu.es>
2023-12-19 20:34:13 +0000

    Pruebas containerBPP

--- src/test/java/es/ull/concrete/metaheuristics/containerBPP/BPPGraspTest.java
+++ src/test/java/es/ull/concrete/metaheuristics/containerBPP/BPPGraspTest.java
@@ -1,6 +1,8 @@
 package es.ull.concrete.metaheuristics.containerBPP;
-
+/**
 import es.ull.abstracts.problem.ILoadingProblems;
+import es.ull.concrete.solutions.BPPcontainerSolution;
+import org.junit.Before;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -29,3 +31,64 @@ class BPPGraspTest {
 
     // Más pruebas pueden ser añadidas aquí si hay otros métodos públicos
 }
+**/
+import es.ull.concrete.Problems.containerBPP.BPPcontainerProblem;
+import es.ull.concrete.metaheuristics.containerBPP.BPPGrasp;
+import es.ull.concrete.solutions.BPPcontainerSolution;
+import es.ull.abstracts.problem.ILoadingProblems;
+import es.ull.abstracts.solution.ILoadingSolutions;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+
+public class BPPGraspTest {
+
+    private ILoadingProblems problem;
+
+    @Before
+    public void setUp() {
+        // Configurar tus instancias de problemas aquí
+        problem = new BPPcontainerProblem(); // Reemplázalo con tu implementación
+    }
+
+    @Test
+    public void testBPPGraspConstruction() {
+        BPPGrasp bppGrasp = new BPPGrasp(problem,0.5, 10);
+        assertNotNull(bppGrasp);
+    }
+
+    @Test
+    public void testBPPGraspSolve() {
+        BPPGrasp bppGrasp = new BPPGrasp(problem, 0.5, 10);
+        assertNotNull(bppGrasp);
+
+        // Asumo que BPPGrasp tiene un método solve que modifica la solución
+        //ILoadingSolutions solution = new BPPcontainerSolution(new int[problem.getNumItems()], problem);
+        bppGrasp.solve(problem);
+
+        // Puedes realizar aserciones según las necesidades de tu aplicación
+        // Por ejemplo, podrías verificar si la solución cumple con ciertas restricciones o tiene un costo aceptable
+        // assertEquals(expectedResult, actualResult);
+    }
+
+    @Test
+    public void testBPPGraspPerformance() {
+        BPPGrasp bppGrasp = new BPPGrasp(problem, 0.5, 10);
+        assertNotNull(bppGrasp);
+
+        // Asegurar que el algoritmo se ejecuta en un tiempo razonable para un conjunto de datos específico
+        long startTime = System.currentTimeMillis();
+        //ILoadingSolutions solution = new BPPcontainerSolution(new int[problem.getNumItems()], problem);
+        bppGrasp.solve(problem);
+        long endTime = System.currentTimeMillis();
+
+        long executionTime = endTime - startTime;
+        System.out.println("BPPGrasp execution time: " + executionTime + " milliseconds");
+
+        // Puedes establecer un límite de tiempo máximo y verificar si la ejecución fue lo suficientemente rápida
+        // assertTrue(executionTime < MAX_EXECUTION_TIME);
+    }
+}
+
