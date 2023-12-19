b8e7757817b65096cffe460f0186dd323d607606
Eduardo José Barrios García <eduardojbarriosgarcia@gmail.com>
2023-12-19 19:43:02 +0000

    adding tests

---
+++ src/test/java/es/ull/concrete/metaheuristics/vehiclesVRP/VRPHillClimbingTest.java
@@ -0,0 +1,21 @@
+package es.ull.concrete.metaheuristics.vehiclesVRP;
+
+import org.junit.jupiter.api.Test;
+
+public class VRPHillClimbingTest {
+
+	@Test
+	void testConstructor() {
+	
+	}
+
+	@Test
+	void testGetBestNeighbour() {
+
+	}
+
+	@Test
+	void testSolve() {
+
+	}
+}
